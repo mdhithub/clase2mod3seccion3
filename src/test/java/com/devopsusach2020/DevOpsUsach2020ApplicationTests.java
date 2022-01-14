ed72801426fc88ad0852dfadd141c79c6741e61f
Roger Reyes <roger.reyes@usach.cl>
2022-01-13 22:42:30 -0300

    Segundo Smell Code

--- src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
+++ src/test/java/com/devopsusach2020/DevOpsUsach2020ApplicationTests.java
@@ -2,12 +2,15 @@ package com.devopsusach2020;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.assertj.core.api.Assertions;
 
 @SpringBootTest
 class DevOpsUsach2020ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String correcto = "hola";
+        Assertions.assertThat(correcto).isEqualTo("hola");
 	}
 
 }
